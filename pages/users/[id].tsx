e8a4bb4b57652b851c26d790ff6cda9605dc822d
Yukako IIDA <yucalcium.c6@gmail.com>
2022-08-13 17:31:56 +0900

    feat: fetchinko from client.

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,18 +1,37 @@
 import { NextPageContext } from 'next'
+import { useEffect, useState } from 'react'
+
+type UserData = {
+  id: string | undefined
+  name: string | undefined
+}
 
 type Props = {
-  userData: {
-    id: string
-    name: string
-  }
+  userData: UserData
 }
 
 const UserPage = ({userData}: Props) => {
+  const [kyoroInfo, setKyoroInfo] = useState<UserData>({
+    id: undefined,
+    name: undefined,
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/hello/kyoro')
+      .then((res) => res.json())
+      .then((data: UserData) => {
+        setKyoroInfo(data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>User info</h1>
       <p>id: {userData.id}</p>
       <p>name: {userData.name}</p>
+      <h1>Kyoro info</h1>
+      <p>id: {kyoroInfo.id}</p>
+      <p>name: {kyoroInfo.name}</p>
     </div>
   )
 }
